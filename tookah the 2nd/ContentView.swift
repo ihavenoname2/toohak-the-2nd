ea5a11c081531b52a1b5d2ad6bd5a0228f87712d
Eugene <eugenetohjj@gmail.com>
2021-09-21 17:19:46 +0800

    Button is thick Button moved to the middle and the texts and the top and bottom app name at the top in bold(toohaK)

--- tookah the 2nd/ContentView.swift
+++ tookah the 2nd/ContentView.swift
@@ -9,27 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
             VStack {
-                Text("toohaK")
-                    .font(.system(size: 90))
-                Text("Not Kahoot")
-                    .font(.system(size: 20))
-                Button("Start!") {
-                    
-                }
-                .background(Color.blue)
-                .foregroundColor(Color.green)
-                .cornerRadius(5)
-                .font(.title)
-                .padding()
-                
-                Text("Press start to begin")
-                    .font(.system(size: 50))
-                    .multilineTextAlignment(.center)
+                    Text("toohaK")
+                        .font(.system(size: 100))
+                        .fontWeight(.bold)
+                    Text("Not Kahoot, totally")
+                        .font(.system(size: 20))
+                        Spacer()
+                    Button("Start!") {
+                        
+                    } .padding(50.0)
+                    .background(Color.blue)
+                    .foregroundColor(Color.green)
+                    .cornerRadius(5)
+                    .frame(width: 300.0, height: 100.0)
+                    .font(.title)
                     .padding()
+                    
+                    Text("Press start to begin")
+                        .font(.system(size: 50))
+                        .multilineTextAlignment(.center)
+                        .padding()
+                Spacer()
             }
-    }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
