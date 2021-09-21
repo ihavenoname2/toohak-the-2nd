9c1ecba7ee6fb2544b19d603b7ca23646c7277c2
Eugene <eugenetohjj@gmail.com>
2021-09-21 16:51:40 +0800

    Initial Commit

---
+++ tookah the 2nd/tookah_the_2ndApp.swift
@@ -0,0 +1,17 @@
+//
+//  tookah_the_2ndApp.swift
+//  tookah the 2nd
+//
+//  Created by sap on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct tookah_the_2ndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
