8139ffa3871152e5f01e22fd7286d7edaf43ede7
Eugene <eugenetohjj@gmail.com>
2021-09-21 17:36:19 +0800

    Dark mode

--- tookah the 2nd/ContentView.swift
+++ tookah the 2nd/ContentView.swift
@@ -9,29 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
             VStack {
-                    Text("toohaK")
-                        .font(.system(size: 100))
-                        .fontWeight(.bold)
-                    Text("Not Kahoot, totally")
-                        .font(.system(size: 20))
-                        Spacer()
-                    Button("Start!") {
-                        
-                    }
-                    .frame(width: 300.0, height: 100.0)
-                    .background(Color.black)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(5)
-                    .font(.system(size: 70))
-                    .padding()
-                    
-                    Text("Press start to begin")
-                        .font(.system(size: 50))
-                        .multilineTextAlignment(.center)
+                        Text("toohaK")
+                            .font(.system(size: 100))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                        Text("Not Kahoot, totally")
+                            .font(.system(size: 20))
+                            .foregroundColor(Color.white)
+                            Spacer()
+                        Button("Start!") {
+                            
+                        }
+                        .frame(width: 300.0, height: 100.0)
+                        .background(Color.white)
+                        .foregroundColor(Color.black)
+                        .cornerRadius(15)
+                        .font(.system(size: 70))
                         .padding()
-                Spacer()
+                        
+                        Text("Press start to begin")
+                            .font(.system(size: 50))
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color.white)
+                            .padding()
+                    Spacer()
             }
+        }.padding().frame(width: 430, height: 910.0) .background(Color.black)
+        
         }
 }
 
